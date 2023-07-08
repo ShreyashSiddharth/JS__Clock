f8371114097614093b1f72c1b4299a1bc552cf6b
ShreyashSiddharth <shreyashsiddharth@gmail.com>
2023-07-08 14:27:21 +0530

    Project Complete

--- proj.js
+++ proj.js
@@ -1,1 +1,12 @@
 //Starting the project
+let timeval;
+let todaysDate;
+let a;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+setInterval(()=>{
+    a = new Date();
+    timeval = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+    
+    todaysDate = a.toLocaleDateString(undefined,options);
+    document.getElementById('time').innerHTML = timeval+ ' on <br>' +todaysDate;
+},1000);
